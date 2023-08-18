cfc9ea69b5ba1e3400389aaaf5e5036cf355987b
NK Test <nkamelin@gmail.com>
2023-08-18 14:11:40 +0300

    done without tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('started!')
+import GameSavingLoader from "./js/GameSavingLoader";
+
+console.log('started!');
+
+GameSavingLoader.load()
+	.then(data => {[]
+		//
+		console.log(JSON.parse(data))
+		return JSON.parse(data);
+	})
+	.catch(err => console.log('Error: ', err))
---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,19 @@
+import json from "./parser";
+import read from "./reader";
+
+export default class GameSavingLoader {
+	// загружает данные (с помощью read), парсит их (с помощью json()) и создаёт объект типа GameSaving
+	static load() {
+
+		return read()
+			.then(data => {
+				return json(data);
+			})
+			.then(json => {
+				//console.log('data received', json);
+				return new Promise(resolve => {
+					resolve(json);
+				});
+			});
+	}
+}
